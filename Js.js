2fa10cf5788725dde9a6c0906e2bde95b37f417e
Andrei Smirnov <to.project.andrey.smirnov@gmail.com>
2019-08-07 14:28:54 +0300

    modern to task2

--- Js.js
+++ Js.js
@@ -11,11 +11,31 @@ let money = +prompt("Ваш бюджет на месяц?", ''),
     income: [],
     savings: false
 };
-let q1=prompt("Введите статью расходов" , ''),
-    q2=prompt("Во скольоко это обойдется?" , ''),
-    q3=prompt("Введите статью расходов" , ''),
-    q4=prompt("Во скольоко это обойдется?" , '');
 
-appData.expenses.q1 = q2;
-appData.expenses.q3 = q4;
-alert(appData.budget / 30);
+for ( let i=0; i<2; i++){
+   let a = prompt("Введите статью расходов", ''),
+    b = prompt("Введите сколько Вы тратите на эту статью", '');
+    if ( typeof(a) === 'string' && typeof(b) != null && typeof(a) != null && 
+    a != '' && b != '') {
+        appData.expenses[a] = b;
+        console.log('done');
+    } else{
+        i--;
+        console.log('bad answer');
+    }
+}
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+
+
+if (appData.moneyPerDay < 100) {
+    console.log ("Это минимальный уровень достатка!");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log ("Это средний уровень достатка!");
+} else if (appData.moneyPerDay > 2000) {
+    console.log ("Это высокий уровень достатка!");
+} else {
+    console.log ("Произошла ошибка");
+}
